920ac1c500ff85deac73459c6d49d768262f24f1
Sascha Greven <sascha.greven.getdatainsight@gmail.com>
2026-07-14 13:26:41 +0200

    fix: v1.6.2 — bashio config paden (modbus.connection_type i.p.v. modbus_connection_type)

--- saldox_ems_bridge/run.sh
+++ saldox_ems_bridge/run.sh
@@ -5,19 +5,19 @@ set -e
 
 bashio::log.info "Saldox EMS Bridge starting…"
 
-export MODBUS_CONNECTION_TYPE="$(bashio::config 'modbus_connection_type' 'tcp')"
-export MODBUS_HOST="$(bashio::config 'modbus_host' '192.168.1.50')"
-export MODBUS_PORT="$(bashio::config 'modbus_port' 502)"
-export MODBUS_SERIAL_PORT="$(bashio::config 'modbus_serial_port' '/dev/ttyUSB0')"
-export MODBUS_BAUDRATE="$(bashio::config 'modbus_baudrate' 9600)"
-export MODBUS_UNIT_ID="$(bashio::config 'modbus_unit_id' 1)"
-export POLL_INTERVAL="$(bashio::config 'modbus_poll_interval_seconds' 10)"
+export MODBUS_CONNECTION_TYPE="$(bashio::config 'modbus.connection_type' 'tcp')"
+export MODBUS_HOST="$(bashio::config 'modbus.host' '192.168.1.50')"
+export MODBUS_PORT="$(bashio::config 'modbus.port' 502)"
+export MODBUS_SERIAL_PORT="$(bashio::config 'modbus.serial_port' '/dev/ttyUSB0')"
+export MODBUS_BAUDRATE="$(bashio::config 'modbus.baudrate' 9600)"
+export MODBUS_UNIT_ID="$(bashio::config 'modbus.unit_id' 1)"
+export POLL_INTERVAL="$(bashio::config 'modbus.poll_interval_seconds' 10)"
 
-export HA_DEVICE_SLUG="$(bashio::config 'ha_device_slug' 'sofar_hyd')"
-export HA_FRIENDLY_NAME="$(bashio::config 'ha_friendly_name' 'Sofar HYD')"
+export HA_DEVICE_SLUG="$(bashio::config 'ha.device_slug' 'sofar_hyd')"
+export HA_FRIENDLY_NAME="$(bashio::config 'ha.friendly_name' 'Sofar HYD')"
 
-export PRICES_SLUG="$(bashio::config 'prices_slug' 'saldox_price')"
-export PRICES_POLL_MINUTES="$(bashio::config 'prices_poll_minutes' 15)"
+export PRICES_SLUG="$(bashio::config 'prices.slug' 'saldox_price')"
+export PRICES_POLL_MINUTES="$(bashio::config 'prices.poll_minutes' 15)"
 
 export SALDOX_API_URL="$(bashio::config 'saldox_api_url' '')"
 export SALDOX_API_TOKEN="$(bashio::config 'saldox_api_token' '')"
